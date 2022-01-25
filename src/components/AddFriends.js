dcfe4083a2717117076f73a596ef4876c3176133
david-aihe <doa9200@gmail.com>
2022-01-25 17:54:21 -0500

    Finished logout functionality

--- src/components/AddFriends.js
+++ src/components/AddFriends.js
@@ -1,23 +1,59 @@
-import React from "react";
+import React, {useState} from "react";
 import loginStyles from './Login.css';
+import { useHistory } from "react-router-dom";
+import axios from "axios";
 
 const AddFriends = () => {
+    const {push} = useHistory();
+    const [ form, setForm] = useState({
+        name: '',
+        email: '',
+    });
+
+    const handleChange = e => {
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const token = localStorage.getItem('token');
+
+        axios.post('http://localhost:9000/api/friends', form, {
+            headers: {
+                authorization: token
+            },
+        })
+        .then(resp=>{
+            push('/friends');
+        })
+        .catch(err=>{
+           console.log(err); 
+        })
+    }
 
     return (
         <div className="forms">
             <h1>Add Friend</h1> 
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="input">
-                    <label htmlFor="friend-name">Friend Name</label>
+                    <label htmlFor="name">Friend Name</label>
                     <input
-                        id="friend-name"
+                        type='text'
+                        name="name"
+                        value= {form.name}
+                        onChange={handleChange}
                     />  
                 </div>
                 <div className="input">
                     <label htmlFor="email">Friend Email</label>
                     <input
                         type='email'
-                        id="email"
+                        name="email"
+                        value={form.email}
+                        onChange={handleChange}
                     /> 
                 </div>
                 <button>Submit</button> 
