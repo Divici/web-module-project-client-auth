0d5625a6e61d46202ccdc7ae1f04adf47372636a
david-aihe <doa9200@gmail.com>
2022-01-25 11:45:53 -0500

    Added styling for Login.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
+import Login from './components/Login';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <h2>Client Auth Project</h2>
+        
         <Route exact path='/'>
-          {/* <Login/> */}
+          <Login/>
         </Route>
         <Route exact path='/login'>
-          {/* <Login/> */}
+          <Login/>
         </Route>
         <Route exact path='/logout'>
           {/* <Logout/> */}
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,30 @@
 import React from "react";
+import loginStyles from './Login.css';
 
 const Login = () => {
 
     return (
         <div>
-            <div className="container">
-                <h1>LOGIN</h1>
+            <div className="login">
+                <div>
+                    <h1>LOGIN</h1>
+                </div>
+                
                 <form>
-                    <h3>USERNAME</h3>
-                    <input></input>
+                    <div className="login-input">
+                        <label htmlFor="username">Username</label>
+                        <input
+                            id="username"
+                        />  
+                    </div>
+                    <div className="login-input">
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type='password'
+                            id="password"
+                        /> 
+                    </div>
+                    <button>Submit</button> 
                 </form>
             </div>
         </div>
